407e9ae5f2ed987849b0bd768d370d9b7b27dc94
Karol Habczyk <karol.habczyk@gmail.com>
2016-11-18 18:37:42 +0100

    Add unit tests for ValidationRule

---
+++ DataGen.Validation.UnitTests/ValidationRuleTests.cs
@@ -0,0 +1,38 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Validation.Contracts;
+using Validation.Model;
+using Validation.Providers;
+
+namespace DataGen.Validation.UnitTests
+{
+    [TestFixture]
+    public class ValidationRuleTests
+    {
+        [Test]
+        public void ValudationRule_Run_InvalidObject_ReturnFalse()
+        {
+            var objectToValidate = new { PropertyToValidate = 3 };
+            var validationRule = new ValidationRule<dynamic>(x => x.PropertyToValidate > 7, "PropertyToValidate has to be grater than seven.");
+            //var validationRules = new List<IValidationRule<dynamic>>() { validationRule };
+            //var validator = Substitute.For<IValidator<dynamic>>(validationRules);
+
+            var actual = validationRule.Run(objectToValidate);
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void ValudationRule_Run_ValidObject_ReturnTrue()
+        {
+            var objectToValidate = new { PropertyToValidate = 9 };
+            var validationRule = new ValidationRule<dynamic>(x => x.PropertyToValidate > 7, "PropertyToValidate has to be grater than seven.");
+
+            var actual = validationRule.Run(objectToValidate);
+
+            Assert.IsTrue(actual);
+        }
+    }
+}
